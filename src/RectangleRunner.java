3f6f9e0cf22d0bb9c4515cd8c792b8017b0571bd
Andy Liang <andyl6@nycstudents.net>
2022-10-03 12:29:31 -0400

    jh

---
+++ src/RectangleRunner.java
@@ -0,0 +1,11 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+
+        Rectangle plot1 = new Rectangle(150, 200);
+        Rectangle plot2 = new Rectangle(125);
+        Rectangle plot3 = new Rectangle();
+
+        System.out.println("Plot 1's length: " + plot1.getLength());
+    }
+}
